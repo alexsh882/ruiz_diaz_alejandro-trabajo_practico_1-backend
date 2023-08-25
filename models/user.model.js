9b98d9d72c4b3edab74df332e1052b5180d75aab
Alejandro <alexsh882@gmail.com>
2023-08-25 20:42:19 -0300

    feat: Se agregaron campos name y last name al modelo user

--- models/user.model.js
+++ models/user.model.js
@@ -2,6 +2,14 @@ import { DataTypes, sequelize } from '../database/config.js';
 import Project from './project.model.js';
 
 const User = sequelize.define('User', {
+  name: DataTypes.STRING,
+  lastName: { type: DataTypes.STRING },
+  fullName: {
+    type: DataTypes.VIRTUAL,
+    get() {
+      return this.lastName + ', ' + this.name
+    }
+  },
   username: DataTypes.STRING,
   password: DataTypes.STRING,  
 }, {
